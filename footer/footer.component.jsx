21a51241a543a6cd16e4e2902a5cb2a15cc9e65f
Jdbain67 <79292888+Jdbain67@users.noreply.github.com>
2021-08-15 10:52:44 -0500

    Add files via upload

---
+++ footer/footer.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./footer.style.css";
+
+const FooterPanel = () => {
+  return (
+    <div>
+      <div className="py-2 text-center footer-style"> <span> <small>&copy; Copyright 2021, JDB </small></span>
+      </div>
+    </div>
+  );
+};
+
+export default FooterPanel;
+
+
